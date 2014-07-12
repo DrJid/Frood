901e6bc528a23bc0fe0e0acef1c1fa96cf96714f
Pako laptop <pako@fluc.com>
2014-07-12 16:10:57 -0700

    filtering by location

--- Frood/ViewController.swift
+++ Frood/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let apiKey = "INSERT-API-KEY"
+    let apiKey = "3a177152804be44404644338256f78"
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        let lat = "37.784296"
+        let lon = "-122.414941"
+        let distance = "5"
         //make the call and the reponse contains the callback json
-        APICLientSingleton.dataTask("/2/open_events.json?topic=photo&time=,1w&key=\(apiKey)", parameters: nil) { response in
+        APICLientSingleton.dataTask("/2/open_events.json?lat=\(lat)&lon=\(lon)&distance=\(distance)&key=\(apiKey)", parameters: nil) { response in
             //response call
             if let response:AnyObject = response  {
              println("repsonse \(response)")
