eed47924ac8f067cc608f893dd394dd6a8922438
Moujaled Maijid <maijid@gmail.com>
2014-07-12 20:20:21 -0700

    win - the worst commit message

---
+++ Frood/Event.swift
@@ -0,0 +1,15 @@
+//
+//  Event.swift
+//  Frood
+//
+//  Created by Maijid Moujaled on 7/12/14.
+//  Copyright (c) 2014 Maijid Moujaled. All rights reserved.
+//
+
+import Foundation
+
+var eventName:String?
+var eventTime:NSDate?
+var eventAddress:String?
+
+
--- Frood/ViewController.swift
+++ Frood/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let apiKey = "761f445110406175175e2272ad16"
     
+    var allEvents = [Event]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,7 +46,63 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         APICLientSingleton.dataTask("/2/open_events.json?lat=\(lat)&lon=\(lon)&distance=\(distance)&key=\(apiKey)", parameters: nil) { response in
             //response call
             if let response:AnyObject = response  {
-                println("repsonse \(response)")
+//                println("repsonse \(response['results'])")
+                
+                var eventsArray:NSArray! = response["results"] as NSArray
+                self.allEvents.removeAll(keepCapacity: false)
+                
+                
+                for i in 0..<eventsArray.count {
+                    let event: NSDictionary! = eventsArray[i] as NSDictionary
+//                    let venue: NSDictionary! = event["venue"] as NSDictionary
+//                    
+//                    if var label = cell.labelTitle{
+//                        label.text = "This is a title"
+//                    }
+                    
+                   
+                    
+                    /*
+                    if let statusesArray = jsonObject as? NSArray{
+                        if let aStatus = statusesArray[0] as? NSDictionary{
+                            if let user = aStatus["user"] as? NSDictionary{
+                                if let userName = user["name"] as? NSDictionary{
+                                    //Finally We Got The Name
+                                    
+                                }
+                            }
+                        }
+                    }
+                    */
+                    
+//                    let group: NSDictionary! = event["group"] as NSDictionary
+                    
+                    
+                    
+//                    var location = venue["address_1"] as String
+                    var name = event["name"] as String
+                    
+//                    let time = event["time"] as time_t;
+//                    let dateFormatter = NSDateFormatter()
+//                    dateFormatter.dateFormat = "yyyy-MM-dd h:mm a" // superset of OP's format
+//                    println("Date: ", dateFormatter.stringFromDate(NSDate()))
+
+                    var e = Event(name: name, location: "My house", time: NSDate.date() )
+                    self.allEvents += e
+                    self.theTableView.reloadData()
+
+                    
+                }
+                
+    
+//                for e in eventsArray {
+//                    let venue: NSDictionary! = e["venue"] as NSDictionary
+//                    var address = venue["address_1"]
+//                    var name = e["venue"] as String
+//                    var event = Event(name: name, location: venue, time: NSDate.date() )
+//                    self.allEvents += event
+//                    self.theTableView.reloadData()
+//                }
             }
         }
     }
@@ -57,17 +115,34 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("eventCell", forIndexPath: indexPath) as EventCell
         
-        cell.typeLabel.text = "haha"
-        cell.eventTitleTime.text = "9am to 9pm"
-        cell.eventAddressLabel.text = "34 Howard"
-        cell.eventTitleLabel.text = "Swift Hackday"
+        var event = self.allEvents[indexPath.row]
+        cell.eventTitleLabel.text = event.eventName
+        cell.eventAddressLabel.text = event.eventLocation
+        cell.typeLabel.text  = event.eventType
+
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.allEvents.count
+
     }
     
+}
+
+
+
+class Event {
+    var eventName = "My Event"
+    var eventLocation = "821 Folsom"
+    var eventTime = NSDate.date()
+    var eventType = "Lunch!"
+    
+    init(name:String, location:String, time:NSDate) {
+        eventName = name
+        eventLocation = location
+        eventTime = time
+    }
 }
 
 
